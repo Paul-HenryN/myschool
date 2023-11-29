d44be91a14c6518b3a0358555206ee2202be5be4
paul-henryN <ngpaulhenry@gmail.com>
2023-11-29 18:41:37 +0100

    Fonctionnalités backend teachers

--- Backend/src/teacher/teacher.data-service.ts
+++ Backend/src/teacher/teacher.data-service.ts
@@ -1,14 +1,154 @@
+import { ExpressDb } from '../infrastructure/express-db';
 import { Teacher } from './teacher';
 import { TeacherService } from './teacher.service';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 
 export class TeacherDataService implements TeacherService {
-    add(username: string): Teacher {
-        throw new Error('Method not yet implemented.');
+    async login(email: string, password: string): Promise<string | null> {
+        try {
+            const query = `SELECT * FROM admins WHERE email = ?`;
+            const result = await ExpressDb.execute(query, [email]);
+
+            if (result && Array.isArray(result) && result.length > 0) {
+                const teacher: Teacher = result[0];
+
+                const isPasswordValid = await bcrypt.compare(
+                    password,
+                    teacher.password,
+                );
+
+                if (isPasswordValid) {
+                    const token = jwt.sign(
+                        { email: teacher.email },
+                        'your-secret-key',
+                        { expiresIn: '1h' },
+                    );
+                    return token;
+                } else {
+                    return null;
+                }
+            } else {
+                return null;
+            }
+        } catch (error) {
+            console.error('Error authenticating user:', error);
+            throw error;
+        }
     }
-    getById(id: number): Teacher | null {
-        throw new Error('Method not implemented.');
+    async add(name: string, email: string, password: string): Promise<Teacher> {
+        try {
+            const hashedPassword = await this.hashPassword(password);
+            // install bcrypt to crypt password
+
+            const Teacher = {
+                name: name,
+                email: email,
+                password: hashedPassword,
+            };
+            const query = `INSERT INTO admins (name, email, password) VALUES ('${name}', '${email}', '${hashedPassword}')`;
+            const result = await ExpressDb.execute(query);
+
+            console.log('Teacher successfully added');
+            return Teacher;
+        } catch (error) {
+            console.error('Error when inserting Teacher.');
+            throw error;
+        }
     }
-    delete(id: number): void {
-        throw new Error('Method not implemented.');
+
+    async updatePassword(
+        email: string,
+        newPassword: string,
+    ): Promise<Teacher | null> {
+        try {
+            const hashedPassword = await this.hashPassword(newPassword);
+
+            const query = `UPDATE admins SET password = ? WHERE email = ?`;
+            const result = await ExpressDb.execute(query, [
+                hashedPassword,
+                email,
+            ]);
+
+            if (
+                result &&
+                result.affectedRows !== undefined &&
+                result.affectedRows > 0
+            ) {
+                console.log('Update OK');
+                const updatedTeacher: Teacher = {
+                    name: '',
+                    email: email,
+                    password: newPassword,
+                };
+                return updatedTeacher;
+            } else {
+                console.log('Teacher not found');
+                return null;
+            }
+        } catch (error) {
+            console.error('Error updating Teacher password:', error);
+            throw error;
+        }
+    }
+
+    async getAllEmail(): Promise<String[] | null> {
+        const query = `SELECT email FROM admins`;
+
+        try {
+            const result = await ExpressDb.execute(query);
+
+            if (result && Array.isArray(result)) {
+                console.log('OK email');
+                console.log(result);
+                const emails = result.map((row: any) => row.email);
+                return emails;
+            } else {
+                console.log('No email');
+                return null;
+            }
+        } catch (error) {
+            console.error(
+                'Erreur lors de la récupération des adresses e-mail des utilisateurs. ',
+                error,
+            );
+            throw error;
+        }
+    }
+
+    async delete(email: string): Promise<string> {
+        const query = `DELETE FROM admins WHERE email = '${email}'`;
+
+        try {
+            const result = await ExpressDb.execute(query);
+
+            if (
+                result &&
+                result.affectedRows !== undefined &&
+                result.affectedRows > 0
+            ) {
+                return 'Teacher deleted successfully';
+            }
+            return "Teacher does'nt exist";
+        } catch (error) {
+            console.error('Error checking and deleting e-mail :', error);
+            throw error;
+        }
+    }
+
+    async getByEmail(email: string): Promise<Teacher | null> {
+        const query = 'SELECT * FROM admins WHERE email = ?';
+        const result = await ExpressDb.execute(query, [email]);
+
+        // Vérifiez si un utilisateur a été trouvé
+        if (result && Array.isArray(result) && result.length > 0) {
+            return result[0]; // Retourne le premier utilisateur trouvé
+        }
+
+        return null; // Aucun utilisateur trouvé avec cet email
+    }
+
+    private async hashPassword(password: string): Promise<string> {
+        return bcrypt.hash(password, 10);
     }
 }
